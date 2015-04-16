c0628a0fc18b5b95fb8b661c13ac9971435cae0c
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-04-16 19:16:39 -0400

    Backbone-ify the Javascript for readability.

--- chrome_app/js/app.js
+++ chrome_app/js/app.js
@@ -66,143 +66,229 @@
         }
 
         if (state == 'bio') {
-            switchSection(currentExpanded.find('.name-section'), true);
+            var bioPanel = _.filter(bioPanels, function(bioPanel) { return bioPanel.el == currentExpanded[0]; })[0];
+            bioPanel.switchSection(currentExpanded.find('.name-section'), true);
         }
     }
 
-    // if they manually jump around, set the state variable right, but don't reset anything
-    panelGroup.on('click', '.panel-title a', function() {
-        currentState = $(this).parents('.panel').eq(0).attr('data-state');
-    })
+    var Panel = Backbone.View.extend({
+        events: {
+            'click .panel-title a': 'makeActive'
+        },
 
-    // deal with all the button event handlers -- so many, geez
+        className: 'panel',
 
-    // find a bio panel
-    panelGroup.on('click', '.find-panel .quit', function() {
-        window.close();
-    })
-    panelGroup.on('click', '.find-panel .new-firm', function() {
-        newFirm();
-    })
-    panelGroup.on('click', '.find-panel .found-one', function() {
-        // remove the current URL from the non-bio list if it's in there
-        visitedNonbio = _.reject(visitedNonbio, function(el) { return el == currentUrl });
-        // and add it to the bio list
-        visitedBio.push(currentUrl);
+        initialize: function(options) {
+            this.options = options || {};
+            this.$el.addClass('panel-default').addClass(this.label + '-panel').attr('data-state', this.label);
+        },
+        
+        render: function() {
+            this.$el.html(tim($('.' + this.label + '-panel-tpl').html(), this.options));
+            return this;
+        },
 
-        // go to the next panel for the first time
-        setState('bio');
-        page.url = currentUrl;
-    })
+        makeActive: function() {
+            currentState = this.$el.attr('data-state');
+        },
 
-    // bio panel
-
-    // sections within the bio panel
-    // -- name section
-    panelGroup.on('click', '.bio-panel .name-section .done', function() {
-        switchSection($('.bio-section'));
+        setFirst: function(isFirst) {
+            this.$el.removeClass(isFirst ? 'not-first': 'first').addClass(isFirst ? 'first' : 'not-first');
+        }
     });
-    // -- bio section
-    panelGroup.on('click', '.bio-panel .bio-section .done', function() {
-        switchSection($('.lobbyist-section'));
-    })
-    panelGroup.on('click', '.bio-panel .bio-section .add', function(evt) {
-        var tpl = $('#chunk-tpl').html();
-        var panel = $(evt.target).parents('.bio-panel');
-        var well = panel.find('.bio-section .well');
-        var selectedType = panel.find('option:selected');
 
-        var rendered = $(tim(tpl, {'type': selectedType.html(), 'body': well.find('.content').html()}));
+    var FindPanel = Panel.extend({
+        label: 'find',
 
-        var selection = well.data('selection');
-        selection.type = selectedType.attr('value');
-        rendered.data('selection', selection);
+        events: _.extend({
+            'click .quit': 'quit',
+            'click .new-firm': newFirm,
+            'click .found-one': 'foundOne'
+        }, Panel.prototype.events),
 
-        panel.find('.saved-chunks').append(rendered);
+        quit: function() {
+            window.close();
+        },
+
+        foundOne: function() {
+            // remove the current URL from the non-bio list if it's in there
+            visitedNonbio = _.reject(visitedNonbio, function(el) { return el == currentUrl });
+            // and add it to the bio list
+            visitedBio.push(currentUrl);
+
+            // go to the next panel for the first time
+            setState('bio');
+            page.url = currentUrl;
+        }     
     });
-    panelGroup.on('click', '.bio-panel .bio-section .chunk .remove', function(evt) {
-        $(evt.target).parents('.chunk').remove();
-    })
-    panelGroup.on('click', '.bio-panel .lobbyist-section .btn', function(evt) {
-        // are they a lobbyist?
-        var $this = $(evt.target);
-        var is_lobbyist = $this.hasClass('yes') ? 'yes' : ($this.hasClass('no') ? 'no' : 'dunno');
 
-        // save their answer into the object
-        $this.parents('.bio-panel').data('is_lobbyist', is_lobbyist);
-        setState('any-more');
-    })
-    var switchSection = function($section, noAnimation) {
-        // delay this to keep button clicks in active sections from triggering the inactive-section click handler
-        setTimeout(function() {
-            var $inactives = $section.parent().find('.panel-section').not($section);
+    var BioPanel = Panel.extend({
+        label: 'bio',
+
+        events: _.extend({
+            'click .name-section .done': 'nameDone',
             
-            // class toggling
-            $inactives.removeClass('panel-section-active').addClass('panel-section-inactive');
-            $section.removeClass('panel-section-inactive').addClass('panel-section-active');
+            'click .bio-section .done': 'bioDone',
+            'click .add': 'addChunk',
 
-            // offset stuff
-            var absOffset = $section.offset();
-            var barOffset = $('.sidebar-top').offset();
-            var chevronOffset = $('.chevron-rel').offset();
+            'click .lobbyist-section .btn': 'decideIfLobbyist',
 
-            // animate the chevron
-            $('.chevron').animate({'top': absOffset.top - chevronOffset.top + parseInt($section.css('padding-top'))});
+            'click .panel-section-inactive': 'pickSection'
+        }, Panel.prototype.events),
 
-            if (!(noAnimation != undefined)) {
-                // scroll the sidebar
-                $('.sidebar').animate({scrollTop: absOffset.top - barOffset.top - 15});
-            }
+        initialize: function(options) {
+            Panel.prototype.initialize.call(this, options);
+            this.bioChunks = [];
+        },
 
-            // toggle form control states
-            $section.find('button,select').removeAttr('disabled');
-            $inactives.find('button,select').attr('disabled', 'disabled');
-        }, 0);
-    }
-    panelGroup.on('click', '.bio-panel .panel-section-inactive', function(evt) {
-        console.log('clicked');
-        var $target = $(evt.target);
-        var $clicked = $target.hasClass('panel-section-inactive') ? $target : $target.parents('.panel-section-inactive').eq(0);
+        nameDone: function() {
+            this.switchSection(this.$('.bio-section'));
+        },
 
-        switchSection($clicked);
-    })
+        bioDone: function() {
+            this.switchSection(this.$('.lobbyist-section'));
+        },
 
-    // any more panel
-    panelGroup.on('click', '.any-more-panel .yes', function() {
-        // go back to the name panel, for the second time
-        $('.bio-panel').removeClass('first').addClass('not-first');
-        setState('bio');
-    })
-    panelGroup.on('click', '.any-more-panel .no', function() {
-        // FIXME: save some shit
-        var toSave = {
-            'url': page.url,
-            'site': page.site,
-            'people': []
+        addChunk: function() {
+            var well = this.$('.bio-section .well');
+            var selectedType = this.$('option:selected');
+
+            var chunk = new BioChunk({'parent': this, 'typeLabel': selectedType.html(), 'typeValue': selectedType.attr('value'), selection: well.data('selection'), 'body': well.find('.content').html()});
+            this.bioChunks.push(chunk);
+
+            this.$('.saved-chunks').append(chunk.render().el);
+        },
+
+        decideIfLobbyist: function(evt) {
+            // are they a lobbyist?
+            var $target = $(evt.target);
+            var is_lobbyist = $target.hasClass('yes') ? 'yes' : ($target.hasClass('no') ? 'no' : 'dunno');
+
+            // save their answer into the object
+            this.options.isLobbyist = is_lobbyist;
+            setState('any-more');
+        },
+
+        switchSection: function($section, noAnimation) {
+            // delay this to keep button clicks in active sections from triggering the inactive-section click handler
+            var _this = this;
+            setTimeout(function() {
+                var $inactives = $section.parent().find('.panel-section').not($section);
+                
+                // class toggling
+                $inactives.removeClass('panel-section-active').addClass('panel-section-inactive');
+                $section.removeClass('panel-section-inactive').addClass('panel-section-active');
+
+                // offset stuff
+                var absOffset = $section.offset();
+                var barOffset = $('.sidebar-top').offset();
+                var chevronOffset = $('.chevron-rel').offset();
+
+                // animate the chevron
+                _this.$('.chevron').animate({'top': absOffset.top - chevronOffset.top + parseInt($section.css('padding-top'))});
+
+                if (!(noAnimation != undefined)) {
+                    // scroll the sidebar
+                    $('.sidebar').animate({scrollTop: absOffset.top - barOffset.top - 15});
+                }
+
+                // toggle form control states
+                $section.find('button,select').removeAttr('disabled');
+                $inactives.find('button,select').attr('disabled', 'disabled');
+            }, 0);
+        },
+
+        pickSection: function(evt) {
+            var $target = $(evt.target);
+            var $clicked = $target.hasClass('panel-section-inactive') ? $target : $target.parents('.panel-section-inactive').eq(0);
+
+            this.switchSection($clicked);
         }
-        // grab all the bio panels
-        $('.bio-panel').each(function() {
-            var $this = $(this);
-            var person = {
-                'name': $this.find('.name-section .well').data('selection'),
-                'bio': [],
-                'is_lobbyist': $this.data('is_lobbyist')
-            }
-            $this.find('.chunk').each(function() {
-                person.bio.push($(this).data('selection'));
-            })
+    });
+    
+    var BioChunk = Backbone.View.extend({
+        events: {
+            'click .remove': 'remove'
+        },
 
-            toSave.people.push(person);
-        })
-        console.log('would save', toSave);
+        initialize: function(options) {
+            this.options = options || {};
+        },
 
-        // go back to the find panel, for the second time
-        $('.find-panel').removeClass('first').addClass('not-first');
-        resetNameFinder();
-        setState('find');
-        // but when we get to the name panel, it'll be the first time there for this page
-        $('.name-panel').removeClass('not-first').addClass('first');
+        render: function() {
+            this.$el.html(tim($('.chunk-tpl').html(), this.options));
+            return this;
+        },
+
+        remove: function() {
+            this.$el.remove();
+            
+            var _this = this;
+            this.options.parent.bioChunks = _.reject(this.options.parent.bioChunks, function(chunk) { return chunk == _this; });
+        }
     })
+
+    var AnyMorePanel = Panel.extend({
+        label: 'any-more',
+
+        events: _.extend({
+            'click .yes': 'pickYes',
+            'click .no': 'pickNo'
+        }, Panel.prototype.events),
+
+        pickYes: function() {
+            _.each(bioPanels, function(bioPanel) {
+                bioPanel.setFirst(false);
+            });
+            setState('bio');
+        },
+
+        pickNo: function() {
+            // FIXME: save some shit
+            var toSave = {
+                'url': page.url,
+                'site': page.site,
+                'people': []
+            }
+            // grab all the bio panels
+            _.each(bioPanels, function(bioPanel) {
+                var person = {
+                    'name': bioPanel.$('.name-section .well').data('selection'),
+                    'bio': [],
+                    'is_lobbyist': bioPanel.options.isLobbyist
+                }
+                _.each(bioPanel.bioChunks, function(chunk) {
+                    var selection = chunk.options.selection;
+                    selection.type = chunk.options.typeValue;
+
+                    person.bio.push(selection);
+                })
+
+                toSave.people.push(person);
+            })
+            console.log('would save', toSave);
+
+            // go back to the find panel, for the second time
+            findPanel.setFirst(false);
+            
+            resetNameFinder();
+            setState('find');
+            
+            // but when we get to the name panel, it'll be the first time there for this page
+            _.each(bioPanels, function(bioPanel) {
+                bioPanel.setFirst(true);
+            });
+        }
+    });
+
+    // make all the panels
+    findPanel = new FindPanel();
+    bioPanels = [new BioPanel()];
+    anyMorePanel = new AnyMorePanel();
+
+    _.each([findPanel, bioPanels[0], anyMorePanel], function(panel) {
+        $('.panel-group').append(panel.render().el);
+    });
 
     // set up event handlers for the iframe
     var webview = $('webview');
