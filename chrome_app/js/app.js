71a995cc5bbbf2affdbeb2f01683221d5c55fcfc
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-04-17 18:00:15 -0400

    Add credit

--- chrome_app/js/app.js
+++ chrome_app/js/app.js
@@ -91,6 +91,7 @@
     }
 
     // utilities
+    // from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
     var uuid = function() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
