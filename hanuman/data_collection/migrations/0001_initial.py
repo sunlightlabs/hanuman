1d4dfa884062e8297b9a351b4d99fb0bcf338f9f
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-04-18 18:30:17 -0400

    Initial commit of the skeleton and models for hanuman data collection.

---
+++ hanuman/data_collection/migrations/0001_initial.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.serializers.json
+import postgres.fields
+from django.conf import settings
+import decimal
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BioPage',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('url', models.URLField()),
+                ('data', postgres.fields.JSONField(encode_kwargs={'cls': django.core.serializers.json.DjangoJSONEncoder}, decode_kwargs={'parse_float': decimal.Decimal})),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Firm',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField()),
+                ('domain', models.TextField()),
+                ('count', models.PositiveIntegerField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Flags',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('type', models.CharField(max_length=32, choices=[(b'not_firm', b'Organization is not a firm'), (b'not_firm_website', b"Not the organization's website"), (b'tech_problem', b'Technical problem with collection'), (b'complete', b'Data collection for this organization is complete')])),
+                ('resolved', models.BooleanField(default=False)),
+                ('notes', models.TextField(blank=True)),
+                ('firm', models.ForeignKey(to='data_collection.Firm')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Session',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('start', models.DateTimeField(auto_now_add=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ViewLog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('bio_pages', postgres.fields.ArrayField(models.URLField(), size=None, blank=True)),
+                ('non_bio_pages', postgres.fields.ArrayField(models.URLField(), size=None, blank=True)),
+                ('firm', models.ForeignKey(to='data_collection.Firm')),
+                ('session', models.ForeignKey(to='data_collection.Session')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='flags',
+            name='session',
+            field=models.ForeignKey(to='data_collection.Session'),
+        ),
+        migrations.AddField(
+            model_name='biopage',
+            name='firm',
+            field=models.ForeignKey(to='data_collection.Firm'),
+        ),
+        migrations.AddField(
+            model_name='biopage',
+            name='session',
+            field=models.ForeignKey(to='data_collection.Session'),
+        ),
+    ]
---
+++ hanuman/data_collection/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+from postgres.fields import JSONField, ArrayField
+
+class Session(models.Model):
+    user = models.ForeignKey(User)
+    start = models.DateTimeField(auto_now_add=True)
+
+class Firm(models.Model):
+    name = models.TextField()
+    domain = models.TextField()
+    count = models.PositiveIntegerField()
+
+FLAG_TYPE_CHOICES = (
+    ('not_firm', 'Organization is not a firm'),
+    ('not_firm_website', 'Not the organization\'s website'),
+    ('tech_problem', 'Technical problem with collection'),
+    ('complete', 'Data collection for this organization is complete'),
+)
+class Flags(models.Model):
+    firm = models.ForeignKey(Firm)
+    session = models.ForeignKey(Session)
+    type = models.CharField(choices=FLAG_TYPE_CHOICES, max_length=32)
+    resolved = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)
+
+class BioPage(models.Model):
+    firm = models.ForeignKey(Firm)
+    session = models.ForeignKey(Session)
+    url = models.URLField()
+    data = JSONField()
+
+class ViewLog(models.Model):
+    firm = models.ForeignKey(Firm)
+    session = models.ForeignKey(Session)
+    bio_pages = ArrayField(models.URLField(), blank=True)
+    non_bio_pages = ArrayField(models.URLField(), blank=True)
