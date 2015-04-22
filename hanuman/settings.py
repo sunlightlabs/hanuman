2afc45c29e60a9baf35e16c21b28d127b9541be9
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-04-21 20:40:53 -0400

    Add machinery for JWT logins.

--- hanuman/settings.py
+++ hanuman/settings.py
@@ -111,8 +111,16 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
 }
+
 
 try:
     from local_settings import *
