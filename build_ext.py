351080be1f40fe33461fbbca8ed501fd9167c4cc
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-04-28 11:48:14 -0400

    Generate Google-compliant version numbers.

--- build_ext.py
+++ build_ext.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import os, subprocess, zipfile, json
-import git
+import os, subprocess, zipfile, json, datetime, time
+import git, dateutil.parser
 
 # get current re
 
@@ -11,9 +11,16 @@ os.chdir(dirname)
 repo = git.Repo("..")
 commit_count = len(list(repo.head.commit.iter_parents())) + 1
 
+# I want to have another value so things generated from branches with the same commit count don't end up with the same ID
+# I'm going somewhat arbitrarily with hours since Jan 1, 2015 when the script is run, because it's monotonically increasing
+# and will fit in Google's 2**16 limit for version ints
+
+hours = int((time.time() - time.mktime(dateutil.parser.parse('2015-01-01').timetuple())) / 3600)
+
 manifest = json.load(open("manifest.json"))
-version = "%s.%s-%s" % (manifest['version'], str(commit_count), repo.head.commit.hexsha[:6])
+version = "%s.%s.%s" % (manifest['version'], str(commit_count), str(hours))
 manifest['version'] = version
+manifest['version_name'] = "%s@%s" % (repo.active_branch.name, repo.head.commit.hexsha)
 
 zipname = "../build/hanuman-build-%s.zip" % version
 subprocess.Popen(["zip", zipname, "-r", ".", "-x", "manifest.json"]).communicate()
