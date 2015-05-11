8eef11d9f61ed44435b6fac49dc5d0ff54bba61b
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-05-11 18:55:11 -0400

    Start to incorporate the machinery for training and classifying.

---
+++ extraction/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+from data_collection.models import Firm
+import uuid
+
+class FirmTrainingSet(models.Model):
+    # use a UUID because we use the ID to name stuff in the filesystem and this ensures uniqueness of files across databases
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    firm = models.ForeignKey(Firm, unique=True)
+
+    spider_complete = models.BooleanField(default=False)
+    page_classifier_trained = models.BooleanField(default=False)
+    element_classifier_trained = models.BooleanField(default=False)
+    extraction_complete = models.BooleanField(default=False)
+
+# this is a single, extracted bio (vs BioPage in data collection, which might have multiple bios on the same page)
+class Bio(models.Model):
+    firm = models.ForeignKey(Firm)
+
+    name = models.TextField()
+    bio_text = ArrayField(models.TextField(), blank=True)
+
+    url = models.URLField()
+    position = models.PositiveIntegerField(default=0)
+
+    # if false, this is the result of a complete manual-tagged firm instead of an ML model extraction
+    automated = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ("url", "position")
---
+++ gevent_manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from gevent import monkey
+monkey.patch_all()
+
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hanuman.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
